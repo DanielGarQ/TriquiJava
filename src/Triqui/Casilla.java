5fd941d15f3b634aa195c0a43fc591d38bfd75fa
Daniel Garcia <danielgarciaquiceno@gmail.com>
2023-02-24 22:58:28 -0500

    Agregué nuevo metodo y cambié tipo de dato

--- src/Triqui/Casilla.java
+++ src/Triqui/Casilla.java
@@ -1,23 +1,27 @@
 package Triqui;
 
 public class Casilla {
-	private int posicion;
-	private char simbolo;
+	private byte posicion;
+	private String simbolo;
 	
 	public void marcar(String simbolo){
 		throw new RuntimeException("NO es posible volver a marcar. Ya existe en esta casilla el simbolo" + getSimbolo());
 	}
 	
+	private boolean cadenaEsNulaOVacia(String cadena) {
+		return  (simbolo == null || "".intern() != simbolo.trim().intern());
+	}
+	
 	public int getPosicion() {
 		return posicion;
 	}
-	public void setPosicion(int posicion) {
+	public void setPosicion(byte posicion) {
 		this.posicion = posicion;
 	}
-	public char getSimbolo() {
+	public String getSimbolo() {
 		return simbolo;
 	}
-	public void setSimbolo(char simbolo) {
+	public void setSimbolo(String simbolo) {
 		this.simbolo = simbolo;
 	}
 
